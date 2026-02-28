ea69069c718479dcb8bd1c14887faa8be3c79c4a
dinhminhcuong27112007-ctrl <dinhminhcuong27112007@gmail.com>
2026-03-01 02:17:32 +0700

    nop lab 7

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Đinh Minh Cường]
+ * MSSV:      [PS48680]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
@@ -10,3 +10,27 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char username[50];
+    char password[50];
+
+    // Nhập username
+    printf("Nhap username: ");
+    scanf("%s", username);
+
+    // Nhập password
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    // Kiểm tra
+    if(strcmp(username, "admin") == 0 && strcmp(password, "123456") == 0) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap khong thanh cong!\n");
+    }
+
+    return 0;
+}
